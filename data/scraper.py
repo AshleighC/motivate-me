756cd6c5b89589f84d8f1b66237a214c2c0b3f8a
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-09-28 16:03:26 -0700

    Changed encoding to Unicode.

--- data/scraper.py
+++ data/scraper.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 from json import dumps
-from unicodedata import normalize
 from urllib import urlopen
-
-def unicode_to_ascii(text):
-  return normalize("NFKD", text).encode("ascii", "ignore")
 
 def get_quote_details(text):
   index = text.rfind(u"–")
-  quote = unicode_to_ascii(text[:index - 1])
-  author = unicode_to_ascii(text[index + 1:])
-  return {"quote": quote, "author": author}
+  return {"quote": text[:index - 1], "author": text[index + 1:]}
 
 def get_quotes():
   url = "http://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/"
