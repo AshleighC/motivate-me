68202476eb48edd7918f1c96eb1ac87e8a9a071c
Elizabeth Lin <elizabethylin@gmail.com>
2013-09-28 15:52:35 -0700

    elizabeth

--- static/js/motivateme.js
+++ static/js/motivateme.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
   var keyPressed = false;
   $('#name-input').putCursorAtEnd();
 
-  $(document).keypress(function() {
+  $(document).keypress(function(evt) {
     if (keyPressed == false) {
       $('#name-input').val('');
       $('#name-input').focus();
+      $('#name-helper-text').show();
       keyPressed = true;
     }
   });
