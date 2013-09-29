6927fc466fbf66b014d8ce945616288930cf1636
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-09-28 21:40:31 -0700

    Testing stuff, ignore ugly code.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from quotes import get_random_quote
+from json import dumps
 
 app = Flask(__name__)
 app.debug = True
@@ -9,8 +10,10 @@ def index():
   if request.method == "GET":
     return render_template("index.html")
   else:
-    name = request.form["name"]
-    color = request.form["color"]
+    # TESTING THINGS... IGNORE UGLY CODE
+    #name = request.form["name"]
+    #color = request.form["color"]
     quote = get_random_quote(request.form["task"])
-    return render_template("quote.html", color=color, name=name, quote=quote)
+    return dumps(quote)
+    #return render_template("quote.html", color=color, name=name, quote=quote)
 
